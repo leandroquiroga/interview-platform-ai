254332f706585536daa265a69ac5e15b259941a2
leandroquiroga <leandroquiroga9514@gmail.com>
2025-06-23 19:44:52 -0300

    Se actualiza el layout de autenticación y el layout de la página de inicio para verificar la autenticación del usuario antes de renderizar el contenido.

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,13 @@
 import Navbar from '@/components/Navbar';
+import { isAuthenticated } from '@/lib/actions/auth.actions';
+import { redirect } from 'next/navigation';
 import React, { ReactNode } from 'react';
 
-const HomeLayout = ({ children }: { children: ReactNode }) => {
+const HomeLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (!isUserAuthenticated) redirect('/sign-in');
+
   return (
     <div className="root-layout">
       <Navbar />
