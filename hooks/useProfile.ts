040c34fc8b68e2c43b9ad5df0c538e7b2a2bbfad
leandroquiroga <leandroquiroga9514@gmail.com>
2025-07-10 18:57:50 -0300

    Elimina las notificaciones de éxito y error en el hook useProfile.

--- hooks/useProfile.ts
+++ hooks/useProfile.ts
@@ -1,6 +1,5 @@
 "use client";
 import { useEffect, useState } from "react";
-import { toast } from "sonner";
 import { userStore } from "@/store/userStore";
 import { getCurrentUser } from "@/utils/functions/auth.action";
 
@@ -14,17 +13,14 @@ export const useProfile = () => {
       const userData = await getCurrentUser();
       if (!userData) {
         setUser(null);
-        toast.error("No user profile found. Please log in.");
         return;
       }
 
       setUser(userData);
       setIsUser(true);
-      toast.success("User profile loaded successfully.");
     } catch (error) {
       console.error("Error fetching user profile:", error);
       setUser(null);
-      toast.error("Error fetching user profile. Please try again later.");
     }
   }
 
