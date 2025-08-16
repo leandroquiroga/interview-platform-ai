a7df0b8520dce9879cd16d17d75b0242e7dad9e7
leandroquiroga <leandroquiroga9514@gmail.com>
2025-08-16 12:48:14 -0300

    Añade componentes Command y Popover con sus respectivas funcionalidades y estilos

---
+++ app/(home)/interview-practice/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import InterviewClient from '@/components/InterviewCliente';
+
+const InterviewPracticePage = () => {
+  return <InterviewClient />;
+};
+
+export default InterviewPracticePage;
